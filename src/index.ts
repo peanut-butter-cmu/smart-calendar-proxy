c0f51a0a2312d253eac107e695e186441ef93a76
onearthit <ceo.perapl@gmail.com>
2025-01-06 17:37:00 +0700

    feat: mango

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import cors from 'cors';
 
 const reg_app = express();
 const oauth_app = express();
+const mango_app = express();
 
 const PORT_REG = process.env.REG_PORT || 3000;
 const PORT_OAUTH = process.env.PORT_OAUTH || 3001;
-const TARGET_URL = '';
+const PORT_MANGO = process.env.PORT_MANGO || 3002;
 const USER_AGENT =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' +
   'AppleWebKit/537.36 (KHTML, like Gecko) ' +
@@ -20,9 +21,11 @@ const CORS = cors({
 
 reg_app.use(CORS);
 oauth_app.use(CORS);
+mango_app.use(CORS);
 
 reg_app.options('*', CORS);
 oauth_app.options('*', CORS);
+mango_app.options('*', CORS);
 
 const createProxyOptions = (target: string) => ({
   target,
@@ -46,13 +49,16 @@ const createProxyOptions = (target: string) => ({
 
 reg_app.use('/', createProxyMiddleware(createProxyOptions('https://www1.reg.cmu.ac.th')));
 oauth_app.use('/', createProxyMiddleware(createProxyOptions('https://oauth.cmu.ac.th')));
+mango_app.use('/', createProxyMiddleware(createProxyOptions('https://mango-cmu.instructure.com')));
 
 reg_app.listen(PORT_REG, () => {
   console.log(`REG Proxy server is running on http://localhost:${PORT_REG}`);
-  console.log(`Forwarding requests to ${TARGET_URL}`);
 });
 
 oauth_app.listen(PORT_OAUTH, () => {
     console.log(`OAuth Proxy server is running on http://localhost:${PORT_OAUTH}`);
-    console.log(`Forwarding requests to ${TARGET_URL}`);
+})
+
+mango_app.listen(PORT_MANGO, () => {
+  console.log(`Mango Proxy server is running on http://localhost:${PORT_MANGO}`);
 })
