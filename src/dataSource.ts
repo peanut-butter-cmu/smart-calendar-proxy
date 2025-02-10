58f063cb72bb5c91479a9fd34b2fe8a735860598
onearthit <ceo.perapl@gmail.com>
2025-02-10 16:04:09 +0700

    load .env in dataSource

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,5 +1,7 @@
+import { configDotenv } from "dotenv";
 import { DataSource } from "typeorm";
 
+configDotenv({ path: ".env" }); // need to load manually.
 export const dataSource = new DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL!,
