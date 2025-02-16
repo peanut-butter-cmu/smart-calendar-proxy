cfc552e7f404e947baec7c913e27c8b20a2f63c5
onearthit <ceo.perapl@gmail.com>
2025-02-16 11:05:21 +0700

    fix no entity not found

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -5,12 +5,6 @@ configDotenv({ path: ".env" }); // need to load manually.
 export const dataSource = new DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL!,
-    entities: [ 
-        (process.env.NODE_ENV === "development") ? 
-        "./src/models/*.entity.ts" : "./dist/models/*.entity.js"
-    ],
-    migrations: [ 
-        (process.env.NODE_ENV === "development") ? 
-        "./src/migrations/*-migration.ts" : "./dist/migrations/*-migration.js"
-    ]
+    entities: [ "./dist/models/*.entity.js" ],
+    migrations: [ "./dist/migrations/*-migration.js" ]
 });
